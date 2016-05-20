1d0077b9f98215dacbfcccd78cf45b7297fddae1
dyu <david.yu.ftw@gmail.com>
2016-05-21 00:33:37 +0800

    deprecate modules

--- protostuff-maven-plugin/src/main/java/com/dyuproject/protostuff/mojo/Module.java
+++ protostuff-maven-plugin/src/main/java/com/dyuproject/protostuff/mojo/Module.java
@@ -14,6 +14,10 @@
 
 package com.dyuproject.protostuff.mojo;
 
+import java.io.File;
+import java.io.Serializable;
+import java.util.Properties;
+
 /**
  * Specifically for the maven-plugin.  
  * This allows you to not specify implementation="com.dyuproject.protostuff.codegen.Module"
@@ -23,12 +27,140 @@ package com.dyuproject.protostuff.mojo;
  * @created Oct 17, 2009
  */
 
-public class Module extends com.dyuproject.protostuff.codegen.Module
+@SuppressWarnings("serial")
+public class Module implements Serializable
 {
 
+    private String fullClassname;
+    private String outputPackage;
+    private String generator;
+    private String encoding;
+
+    private File outputDir;
+    
+    private Properties options = new Properties();
+    
+    public Module()
+    {
+        
+    }
+    
+    public Module(String fullClassname, String outputPackage, 
+            String generator, String encoding, File outputDir)
+    {
+        this.fullClassname = fullClassname;
+        this.outputPackage = outputPackage;
+        this.generator = generator;
+        this.encoding = encoding;
+        this.outputDir = outputDir;
+    }
+    
+    public Module(String fullClassname, String outputPackage, 
+            String generator, String encoding, File outputDir, Properties options)
+    {
+        this(fullClassname, outputPackage, generator, encoding, outputDir);
+        this.options = options;
+    }
+    
     /**
-     * 
+     * @return the fullClassname
      */
-    private static final long serialVersionUID = 5315467076398259016L;
+    public String getFullClassname()
+    {
+        return fullClassname;
+    }
+    /**
+     * @param fullClassname the fullClassname to set
+     */
+    public void setFullClassname(String fullClassname)
+    {
+        this.fullClassname = fullClassname;
+    }
+    /**
+     * @return the outputPackage
+     */
+    public String getOutputPackage()
+    {
+        return outputPackage;
+    }
+    /**
+     * @param outputPackage the outputPackage to set
+     */
+    public void setOutputPackage(String outputPackage)
+    {
+        this.outputPackage = outputPackage;
+    }
+
+    /**
+     * @return the generator
+     */
+    public String getGenerator()
+    {
+        return generator;
+    }
+
+    /**
+     * @param generator the generator to set
+     */
+    public void setGenerator(String generator)
+    {
+        this.generator = generator;
+    }
+    
+    /**
+     * @return the encoding
+     */
+    public String getEncoding()
+    {
+        return encoding;
+    }
+
+    /**
+     * @param encoding the encoding to set
+     */
+    public void setEncoding(String encoding)
+    {
+        this.encoding = encoding;
+    }
+    
+    /**
+     * @return the outputDir
+     */
+    public File getOutputDir()
+    {
+        return outputDir;
+    }
+    /**
+     * @param outputDir the outputDir to set
+     */
+    public void setOutputDir(File outputDir)
+    {
+        this.outputDir = outputDir;
+    }
+    
+    /**
+     * @return the options
+     */
+    public Properties getOptions()
+    {
+        return options;
+    }
+    /**
+     * @param options
+     */
+    public void setOptions(Properties options)
+    {
+        this.options.putAll(options);
+    }
+    
+    public String getOption(String key)
+    {
+        return options.getProperty(key);
+    }
+    
+    public void setOption(String key, String value)
+    {
+        options.setProperty(key, value);
+    }
 
 }
